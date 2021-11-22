bb1b1ce2e9890c8e6136a898e1d56300b0194072
Evgeny Nerush <eugene.nerush@gmail.com>
2021-11-22 22:59:37 +0300

    one more iteration...

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,8 @@
-use morpho::{X, Y, Z, SimpleV3};
+use morpho::{X, Y, Z, Vector_3};
 
 fn main() {
     println!("Hello, world!");
-    let mut v = SimpleV3 { x: 1.0, y: 2.0, z: 3.0 };
+    let mut v = Vector_3 { x: 1.0, y: 2.0, z: 3.0 };
     println!("x, y, z = {}, {}, {}", v.x, v.y, v.z);
     v.x = 8.0;
     println!("x, y, z = {}, {}, {}", v.x, v.y, v.z);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // the idea behind these traits is that
 // (1) this is a general interface for 1d, 2d, 3d vectors and
 // (2) vectors can be stored quite arbitrary in the memory, opposite to plain {x, y, z} structures,
-//     that can yield better performance
+//     that potentially can lead to better performance
 
 pub trait X { fn x(&self) -> &f64; }
 pub trait Y { fn y(&self) -> &f64; }
@@ -14,16 +14,21 @@ pub trait Vector3: X + Y + Z {}
     fn b(&self) -> &Vector3;
 }*/
 
-// implementation
+//
+// Implementation. To avoid confusion between the trait and structures which implement it,
+// the structures always have "_" (underscore) in their names.
+//
 
-pub struct SimpleV3 { pub x: f64, pub y: f64, pub z: f64 }
-impl Vector3 for SimpleV3 {}
-impl X for SimpleV3 { fn x(&self) -> &f64 { &self.x } }
-impl Y for SimpleV3 { fn y(&self) -> &f64 { &self.y } }
-impl Z for SimpleV3 { fn z(&self) -> &f64 { &self.z } }
+pub struct Vector_3 { pub x: f64, pub y: f64, pub z: f64 }
+impl Vector3 for Vector_3 {}
+impl X for Vector_3 { fn x(&self) -> &f64 { &self.x } }
+impl Y for Vector_3 { fn y(&self) -> &f64 { &self.y } }
+impl Z for Vector_3 { fn z(&self) -> &f64 { &self.z } }
 
-/*struct SimpleF3 { pub e: SimpleV3, pub b: SimpleV3 }
-impl Field3 for SimpleV3 {
+/*struct SimpleF3 { pub e: Vector_3, pub b: Vector_3 }
+impl Field3 for Vector_3 {
     fn e(&self) -> &Vector3 { &self.e }
     fn b(&self) -> &Vector3 { &self.b }
 }*/
+
+// ok, that if we need different dimensions, e.g. particle with (x,y,x) and (py, pz)?
